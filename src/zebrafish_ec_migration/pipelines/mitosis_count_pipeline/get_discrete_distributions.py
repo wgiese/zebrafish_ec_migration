8c160b148b1a685cee81808e9e127b38335f5496
wgiese <wolfgang.giese@mdc-berlin.de>
2021-02-23 19:25:56 +0100

    added extraction of json for mitosis-migration-count analysis

--- src/zebrafish_ec_migration/pipelines/mitosis_count_pipeline/get_discrete_distributions.py
+++ src/zebrafish_ec_migration/pipelines/mitosis_count_pipeline/get_discrete_distributions.py
@@ -24,7 +24,7 @@ def get_migration_distributions(preprocessed_key_file: pd.DataFrame, processed_m
     analysis_groups = parameters["analysis_groups"]
     mitosis_events = np.arange(5)
 
-    attribute_list = ["cell_number","mitosis_events","migration_to_dlav","migration_from_dlav"]
+    attribute_list = ["cell_number", "mitosis_events", "migration_to_dlav", "migration_from_dlav"]
     for attribute in attribute_list:
 
         distributions_dict = dict()
@@ -84,4 +84,28 @@ def get_migration_distributions(preprocessed_key_file: pd.DataFrame, processed_m
 
         migration_dict[attribute] = distributions_dict
 
+    print(migration_dict)
+
     return migration_dict
+
+def _extract_mitosis_info(preprocessed_key_file: pd.DataFrame, processed_mitosis_migration_file: pd.DataFrame, mitosis_events,
+                        vessel_type, analysis_group="control", dpf=1):
+
+    preprocessed_key_file = preprocessed_key_file[preprocessed_key_file["analysis_group"] == analysis_group]
+    preprocessed_key_file = preprocessed_key_file[preprocessed_key_file["dpf"] == dpf]
+
+    mitosis_events_ISV = np.zeros(len(mitosis_events))
+
+    ISV_df_all = pd.DataFrame()
+
+    for fish_number in preprocessed_key_file["fish number"].unique():
+        mitosis_migration_fish = processed_mitosis_migration_file[
+            processed_mitosis_migration_file["fish_number"] == fish_number]
+        ISV_df = mitosis_migration_fish[mitosis_migration_fish["vessel_ID"].str.contains(vessel_type)]
+
+        ISV_df_all = ISV_df_all.append(ISV_df, ignore_index=True)
+
+        for events in mitosis_events:
+            mitosis_events_ISV[events] += len(ISV_df[ISV_df["mitosis_events"] == events])
+
+    return ISV_df_all, mitosis_events_ISV
--- src/zebrafish_ec_migration/pipelines/mitosis_count_pipeline/pipeline.py
+++ src/zebrafish_ec_migration/pipelines/mitosis_count_pipeline/pipeline.py
@@ -4,6 +4,11 @@ from zebrafish_ec_migration.pipelines.mitosis_count_pipeline.process_mitosis_mig
     process_mitosis_migration_file,
 )
 
+from zebrafish_ec_migration.pipelines.mitosis_count_pipeline.get_discrete_distributions import (
+    get_migration_distributions,
+)
+
+
 def create_pipeline(**kwargs):
 
     return Pipeline(
@@ -11,7 +16,8 @@ def create_pipeline(**kwargs):
 	    #convert file to csv
 	    node(process_mitosis_migration_file, ["imaris_key_file", "preprocessed_mitosis_migration_file", "parameters"] ,
              ["processed_migration_file", "migration_statistics_file"], name="process_key_file"),
-	    #process file
-	    #node(process_mitosis_migration_file, "parameters", "imaris_key_file", name="process_key_file"),
+        node(get_migration_distributions,
+                 ["imaris_key_file", "processed_migration_file", "parameters"],
+                 "migration_distributions", name="get_migration_distributions"),
         ]
     )
